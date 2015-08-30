f430c4aecefe88fd1c01db723c02b05d448c0cb7
sergey-lance <sergey-lance@ya.ru>
2015-08-31 01:57:46 +0300

    SQL ALLCAPS style

--- tournament.sql
+++ tournament.sql
@@ -6,27 +6,22 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
-drop table players cascade;
-drop table matches cascade;
+DROP TABLE players CASCADE;
+DROP TABLE matches CASCADE;
 
-create table players (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(128) NOT NULL UNIQUE
-	);
-	
-create table matches (
-	player int REFERENCES players(id) ON DELETE CASCADE,
-	rival int REFERENCES players(id) ON DELETE CASCADE,
-	score smallint,
-	PRIMARY KEY (player, rival)
-	);
+CREATE TABLE players ( id SERIAL PRIMARY KEY, name VARCHAR(128) NOT NULL UNIQUE );
 
--- just for fun, not used in python code
-create view scoretable
-as 
-SELECT players.id, players.name, SUM(matches.score) as total,
-	sum(CASE WHEN matches.score != 0 THEN 1 END) AS mw
-	FROM players
-	left outer JOIN matches ON players.id=matches.player
-	GROUP BY players.id
-	ORDER BY total DESC;
+CREATE TABLE matches ( player int REFERENCES players(id) ON
+                      DELETE CASCADE, rival int REFERENCES players(id) ON
+                      DELETE CASCADE, score smallint, PRIMARY KEY (player, rival) );
+
+
+CREATE VIEW scoretable AS
+SELECT players.id,
+       players.name,
+       SUM(matches.score) AS total,
+       sum(CASE WHEN matches.score != 0 THEN 1 END) AS mw
+FROM players
+LEFT OUTER JOIN matches ON players.id=matches.player
+GROUP BY players.id
+ORDER BY total DESC;
